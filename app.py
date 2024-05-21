d4e57869c5b15e7b9ad65676453224a88a2dc6df
Anmolsarrafbest <Anmolsarraf2975@gmail.com>
2024-05-21 13:20:27 +0530

    some more pages and codes where added

--- app.py
+++ app.py
@@ -10,30 +10,36 @@ db = SQLAlchemy()
 db.init_app(app)
 app.app_context().push()
 
+# defining models
+
 class user(db.Model):
     __tablename__='user'
     user_id=db.Column(db.Integer,primary_key=True,autoincrement=True)
-    username=db.Column(db.String, unique=True)
-    email=db.Column(db.String, unique=True)
-    password=db.Column(db.String)
+    username=db.Column(db.String, unique=True,nullable=False)
+    password=db.Column(db.String,nullable=False)
+
 class influencer(db.Model):
     __tablename__='influcener'
     influencer_id=db.Column(db.Integer,primary_key=True,autoincrement=True)
-    name=db.Column(db.String)
+    name=db.Column(db.String,db.ForeignKey(user.username))
     Category=db.Column(db.String)
     Niche=db.Column(db.String)
     reach=db.Column(db.Integer)
+    user_id=db.Column(db.Integer,db.ForeignKey(user.user_id))
+
 class Sponsers(db.Model):
     __tablename__='sponsers'
     Sponsers_id=db.Column(db.Integer,primary_key=True,autoincrement=True)
     Company_name=db.Column(db.String)
     industry=db.Column(db.String)
     budget=db.Column(db.Integer)
+    user_id=db.Column(db.Integer,db.ForeignKey(user.user_id))
+
 class campaigns(db.Model):
     __tablename__='campaign'
     campaigns_id=db.Column(db.Integer,primary_key=True,autoincrement=True)
-    sponser_id=db.Column(db.String,db.Foreigen_key(Sponsers.Sponsers_id))
-    name=db.Column(db.string,nullable=False)
+    sponser_id=db.Column(db.String,db.ForeignKey(Sponsers.Sponsers_id))
+    name=db.Column(db.String,nullable=False)
     description=db.Column(db.String)
     start_date=db.Column(db.String)
     end_date=db.Column(db.String)
@@ -41,18 +47,31 @@ class campaigns(db.Model):
     Visibility=db.Column(db.String)
     goals=db.Column(db.String)
 
-class Ad_request(db.model):
+class Ad_request(db.Model):
     __tablename__="ad_req"
-    campaigns_id=db.Column(db.Integer,db.Foreigen_key(campaigns.campaigns_id))
-    sponsers_id=db.Column(db.Integer,db.Foregin_key(Sponsers.Sponsers_id))
+    adreq_id=db.Column(db.Integer,primary_key=True,autoincrement=True)
+    campaigns_id=db.Column(db.Integer,db.ForeignKey(campaigns.campaigns_id))
+    sponsers_id=db.Column(db.Integer,db.ForeignKey(Sponsers.Sponsers_id))
     messages=db.Column(db.String)
     requirements=db.Column(db.String)
     payment_amount=db.Column(db.String)
     status=db.Column(db.String)
-    
+
 @app.route("/",methods=["GET"])
 def home():
-    return render_template("index.html")
+    return render_template("logging.html")
+
+@app.route("/",methods=['POST'])
+def returns():
+    username=request.form['username']
+    password=request.form['pass']
+    role=request.form['role']
+    if role =="Admin":
+        return render_template('admin.html')
+    elif role=="Sponsers":
+        return render_template("sponser.html")
+    else:
+        return render_template('user.html')
 
 if __name__=="__main__":
     app.run(debug=True)
